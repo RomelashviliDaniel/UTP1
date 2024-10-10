1bf50ba409ab97ec90f40b13cfa17435b5a69a27
RomelashviliDaniel <s31393@pjwstk.edu.pl>
2024-10-10 16:11:06 +0200

    A

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        Adder adder = new Adder();
+        System.out.println(adder.add(1, 2));
+        Subtractor subtractor = new Subtractor();
+        U T P T a s k 1 – G i t w o r k f l o w 2
+        Filip Kwiatkowski, fk@pjwstk.edu.pl
+        7
+        8
+        9
+        System.out.println(subtractor.subtract(6, 3));
+    }
+}
+}
